f6b802539c2a9a1ae8570d85b6ecc251b4026193
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-05-16 00:53:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deployment-notifier/',
   plugins: [react()],
+  base: '/deployment-notifier/',
   optimizeDeps: {
-    exclude: ['lucide-react']
-  }
+    exclude: ['lucide-react'],
+  },
 });
